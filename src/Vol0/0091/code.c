f7eb0ddd76147df9fba055b7fd7072b8227d4991
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-22 16:39:34 +0900

    0091 データ読み込み実装

--- src/Vol0/0091/code.c
+++ src/Vol0/0091/code.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
+int n,cloth[10][10];
+
 int main()
 {
+  int i,j,amount=0;
+  scanf("%d",&n);
+  for(i=0; i<10; ++i){
+    for(j=0; j<10; ++j){
+      scanf("%d", &cloth[i][j]);
+      amount += cloth[i][j];
+    }
+  }
+  printf("%d\n", amount);
   return 0;
 }
