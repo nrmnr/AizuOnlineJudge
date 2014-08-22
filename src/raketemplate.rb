308f87cd2de9bdfe3465a3ac0635e567423c549c
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-22 09:55:31 +0900

    rakefileをWindows対応にする

--- src/raketemplate.rb
+++ src/raketemplate.rb
@@ -5,7 +5,21 @@ require "rake/clean"
 CLOBBER << "code.out"
 CLOBBER << FileList["actual*.txt"]
 
-rule ".out" => [".c"] do |t|
+def is_win?
+  return !!(RUBY_PLATFORM =~ /mswin(?!ce)|mingw|cygwin|bccwin/)
+end
+
+if is_win?
+  $ext = ".exe"
+  $exe = "code.exe"
+  $pre = ""
+else
+  $ext = ".out"
+  $exe = "code.out"
+  $pre = "./"
+end
+
+rule $ext => [".c"] do |t|
   sh "gcc #{t.source} -o #{t.name}"
 end
 
@@ -15,11 +29,11 @@ Dir.glob("expected*.txt").each do |expected|
   actual = "actual#{num}.txt"
   testcase = "testcase#{num}.txt"
 
-  file actual => "code.out" do
+  file actual => $exe do
     if File.exist? testcase
-      sh "./code.out < #{testcase} > #{actual}"
+      sh "#{$pre}#{$exe} < #{testcase} > #{actual}"
     else
-      sh "./code.out > #{actual}"
+      sh "#{$pre}#{$exe} > #{actual}"
     end
   end
 
