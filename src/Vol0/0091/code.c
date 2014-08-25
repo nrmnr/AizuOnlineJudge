e4d85021bdea71f9bb7068395377a43b04707df0
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-25 20:36:32 +0900

    refact: 0091 滴下可能範囲に関する処理を簡略化
    Time Limit Exceeded.

--- src/Vol0/0091/code.c
+++ src/Vol0/0091/code.c
@@ -10,13 +10,9 @@ struct {
   {0,-2}, {2,0}, {0,2}, {-2,0}
 };
 
-struct {
-  int low;
-  int up;
-  int blur;
-} drop_size[] = {
-  {0, 0, 0}, // dummy
-  {1, 9, 5}, {1, 9, 9}, {2, 8, 13}
+int blur_count[] = {
+  0, // dummy
+  5, 9, 13
 };
 
 int cloth[10][10];
@@ -29,7 +25,7 @@ void drop(int x, int y, int delta)
 void drop_ink(int x, int y, int size, int delta)
 {
   int i;
-  for(i=0; i<drop_size[size].blur; ++i){
+  for(i=0; i<blur_count[size]; ++i){
     drop(x+drop_pts[i].dx, y+drop_pts[i].dy, delta);
   }
 }
@@ -37,9 +33,9 @@ void drop_ink(int x, int y, int size, int delta)
 int enable_drop(int x, int y, int size)
 {
   int i;
-  if(x<drop_size[size].low || drop_size[size].up<=x ||
-     y<drop_size[size].low || drop_size[size].up<=y) return 0;
-  for(i=0; i<drop_size[size].blur; ++i){
+  for(i=0; i<blur_count[size]; ++i){
+    if(x+drop_pts[i].dx < 0 || 10 <= x+drop_pts[i].dx ||
+       y+drop_pts[i].dy < 0 || 10 <= y+drop_pts[i].dy) return 0;
     if(cloth[y+drop_pts[i].dy][x+drop_pts[i].dx] <= 0) return 0;
   }
   return 1;
@@ -48,22 +44,22 @@ int enable_drop(int x, int y, int size)
 int solve(int remain_drop, int blur_amount)
 {
   if(remain_drop == 0) return (blur_amount == 0);
-  if(blur_amount < remain_drop * drop_size[1].blur) return 0;
-  if(blur_amount > remain_drop * drop_size[3].blur) return 0;
+  if(blur_amount < remain_drop * blur_count[1]) return 0;
+  if(blur_amount > remain_drop * blur_count[3]) return 0;
 
   int x,y,size;
   for(y=1; y<9; ++y){
     for(x=1; x<9; ++x){
       if(cloth[y][x] <= 0) continue;
       for(size=3; size>=1; --size){
-        if(blur_amount < drop_size[size].blur) continue;
+        if(blur_amount < blur_count[size]) continue;
         if(!enable_drop(x, y, size)) continue;
         drop_ink(x, y, size, -1);
         if(cloth[y][x] < cloth[y-1][x] || (y >= 2 && cloth[y-1][x] < cloth[y-2][x])){
           drop_ink(x, y, size, 1); // recover
           return 0;
         }
-        if(!solve(remain_drop-1, blur_amount-drop_size[size].blur)){
+        if(!solve(remain_drop-1, blur_amount-blur_count[size])){
           drop_ink(x, y, size, 1); // recover
         } else {
           printf("%d %d %d\n", x, y, size);
