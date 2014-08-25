717dc73ee75b8a16d1c523daa7cc4312fa0a22a0
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-25 21:08:34 +0900

    refact: 0091 滴下位置の探索を修正
    Accepted.

--- src/Vol0/0091/code.c
+++ src/Vol0/0091/code.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-struct {
+typedef struct {
   int dx;
   int dy;
-} drop_pts[] = {
+} Point;
+Point drop_pts[] = {
   {0,0}, {0,-1}, {1,0}, {0,1}, {-1,0},
   {1,-1}, {1,1}, {-1,1}, {-1,-1},
   {0,-2}, {2,0}, {0,2}, {-2,0}
+};
+Point drop_target[] = {
+  {0, 0}, // dummy
+  {0, 1}, {1, 1}, {0, 2}
 };
 
 int blur_count[] = {
@@ -41,33 +46,42 @@ int enable_drop(int x, int y, int size)
   return 1;
 }
 
+int find_first_blur(int *px, int *py)
+{
+  int x, y;
+  for(y=0; y<10; ++y)
+    for(x=0; x<10; ++x)
+      if(cloth[y][x] > 0){
+        *px = x;
+        *py = y;
+        return 1;
+      }
+  return 0;
+}
+
 int solve(int remain_drop, int blur_amount)
 {
   if(remain_drop == 0) return (blur_amount == 0);
   if(blur_amount < remain_drop * blur_count[1]) return 0;
   if(blur_amount > remain_drop * blur_count[3]) return 0;
 
-  int x,y,size;
-  for(y=1; y<9; ++y){
-    for(x=1; x<9; ++x){
-      if(cloth[y][x] <= 0) continue;
-      for(size=3; size>=1; --size){
-        if(blur_amount < blur_count[size]) continue;
-        if(!enable_drop(x, y, size)) continue;
-        drop_ink(x, y, size, -1);
-        if(cloth[y][x] < cloth[y-1][x] || (y >= 2 && cloth[y-1][x] < cloth[y-2][x])){
-          drop_ink(x, y, size, 1); // recover
-          return 0;
-        }
-        if(!solve(remain_drop-1, blur_amount-blur_count[size])){
-          drop_ink(x, y, size, 1); // recover
-        } else {
-          printf("%d %d %d\n", x, y, size);
-          return 1;
-        }
-      }
+  int x,y,fx,fy,size;
+  if(!find_first_blur(&fx,&fy)) return 0;
+
+  for(size=3; size>=1; --size){
+    if(blur_amount < blur_count[size]) continue;
+    x = fx + drop_target[size].dx;
+    y = fy + drop_target[size].dy;
+    if(!enable_drop(x, y, size)) continue;
+    drop_ink(x, y, size, -1);
+    if(!solve(remain_drop-1, blur_amount-blur_count[size])){
+      drop_ink(x, y, size, 1); // recover
+    } else {
+      printf("%d %d %d\n", x, y, size);
+      return 1;
     }
   }
+
   return 0;
 }
 
