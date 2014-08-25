967e98c7bf82aea2605cf7e6f218e5f2c233fc7c
nrmnr <n.pujita+github@gmail.com>
2014-08-26 01:05:17 +0900

    fix: 0092 行取得用バッファサイズ修正
    テストケースの改行コードがCRLF?

--- src/Vol0/0092/code.c
+++ src/Vol0/0092/code.c
@@ -16,11 +16,11 @@ int min3(int x, int y, int z)
 int main()
 {
   int i,j,n,s;
-  char line[1002];
+  char line[1003];
   for(;scanf("%d\n", &n) && n;){
     s = 0;
     for(i=1; i<=n; ++i){
-      fgets(line, 1001, stdin);
+      fgets(line, 1002, stdin);
       for(j=1; j<=n; ++j){
         cells[i][j] = (line[j-1] == '.')? min3(cells[i-1][j-1], cells[i-1][j], cells[i][j-1]) + 1 : 0;
         if(s < cells[i][j]) s = cells[i][j];
