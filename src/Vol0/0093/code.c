c6885940005131344e1d78181fe1f2ca9baf344e
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-26 09:03:07 +0900

    0093 実装
    Accepted.

--- src/Vol0/0093/code.c
+++ src/Vol0/0093/code.c
@@ -2,5 +2,17 @@
 
 int main()
 {
+  int y,ye,c,dc;
+  for(dc=0;scanf("%d%d",&y,&ye) && (y || ye);++dc){
+    if(dc) puts("");
+    if(y%4) y=(y/4+1)*4;
+    for(c=0;y<=ye;y+=4){
+      if(y%400==0 || y%100!=0){
+        printf("%d\n",y);
+        ++c;
+      }
+    }
+    if(!c) puts("NA");
+  }
   return 0;
 }
