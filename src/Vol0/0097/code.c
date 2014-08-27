83ed7437e661dfe112f69cadc841ea70500a15c0
n-fujita <n-fujita@daiichi-g.co.jp>
2014-08-27 09:41:29 +0900

    0097 実装
    Time Limit Exceeded.

--- src/Vol0/0097/code.c
+++ src/Vol0/0097/code.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 
-int flags[101];
+int up_bounds[] = {-1,100,199,297,394,490,585,679,772,864};
 
-int min(int x, int y){ return (x < y)? x : y; }
-void prt()
+int solve(int n, int s, int count, int low_bound)
 {
+  if(n == 0) return (s == 0)? count+1 : count;
+  if(s > up_bounds[n]) return count;
   int i;
-  for(i=0;i<100;++i){
-    if(flags[i]) fprintf(stderr, "+%d", i);
-  }
-  fprintf(stderr, "\n");
-}
-
-int solve(int n, int s, int count, int upper)
-{
-  if(n==1){
-    if(flags[s] == 0){
-      flags[s] = 1;
-      prt();
-      flags[s] = 0;
-      return count+1;
-    } else {
-      return count;
-    }
-  }
-  int i;
-  for(i=min(s,upper);i>s/2;--i){
-    flags[i] = 1;
-    count = solve(n-1, s-i, count, i-1);
-    flags[i] = 0;
+  for(i=low_bound;i<=100 && i<=s;++i){
+    count = solve(n-1, s-i, count, i+1);
   }
   return count;
 }
@@ -37,13 +17,7 @@ int main()
 {
   int i,n,s,c;
   for(;scanf("%d%d",&n,&s) && (n||s);){
-    fprintf(stderr, "%d %d\n", n, s);
-    if(s > 864){
-      puts("0");
-      continue;
-    }
-    for(i=0;i<=100;++i) flags[i] = 0;
-    printf("%d\n", solve(n, s, 0, 100));
+    printf("%d\n", solve(n, s, 0, 0));
   }
   return 0;
 }
