f7c567042b0b4d2725aec1bc835fc15a2398cec6
n-fujita <n-fujita@daiichi-g.co.jp>
2014-09-02 18:50:31 +0900

    fix 0099 セグメント木を使ったロジックに変更

--- src/Vol0/0099/code.c
+++ src/Vol0/0099/code.c
@@ -1,53 +1,66 @@
 #include <stdio.h>
+#include <limits.h>
 
-#define N 1000001
+#define N (1<<20)
 
-struct TAG {
-  int num;
-  int val;
-  struct TAG *next;
-};
-struct TAG dp[N];
+int st[N*2]; // segment tree
+int dt_n, st_n;
 
-void ins(int a, int v)
+int max(int x, int y){ return (x > y)? x : y; }
+
+void restruct(int i)
 {
-  dp[a].val += v;
-  if(!dp->next){
-    dp->next = &dp[a];
-    return;
+  while(i>0){
+    i=(i-1)/2;
+    st[i] = max(st[i*2+1], st[i*2+2]);
   }
+}
 
-  struct TAG *t;
+void set(int a, int v)
+{
+  int i = st_n + a - 1;
+  st[i] = v;
+  restruct(i);
+}
 
-  for(t=dp;t->next;t=t->next){
-    if(a == t->next->num){
-      t->next = t->next->next;
-      break;
+void add(int a, int v)
+{
+  int i = st_n + a - 1;
+  st[i] += v;
+  restruct(i);
+}
+
+void init()
+{
+  int i;
+  for(st_n=1;st_n<dt_n;st_n*=2);
+  for(i=0;i<st_n*2-1;++i) st[i] = INT_MIN;
+  for(i=0;i<dt_n;++i) set(i,0);
+}
+
+int get_id()
+{
+  int i=1, j=2;
+  for(;;j=i+1){
+    if(st[i] >= st[j]){
+      if(i >= st_n-1) return i-st_n+1;
+      i = 2*i+1;
+    } else {
+      if(j >= st_n-1) return j-st_n+1;
+      i = 2*j+1;
     }
   }
-
-  int val;
-  for(t=dp;t->next;t=t->next){
-    val = t->next->val;
-    if(dp[a].val > val || (dp[a].val == val && a < t->next->num)){
-      dp[a].next = t->next;
-      t->next = &dp[a];
-      return;
-    }
-  }
-  t->next = &dp[a];
-  dp[a].next = NULL;
 }
 
 int main()
 {
-  int n,q,a,v,i;
-  for(i=0;i<N;++i) dp[i].num = i;
-  scanf("%d%d",&n,&q);
+  int q,a,v;
+  scanf("%d%d",&dt_n,&q);
+  init();
   for(0;q-->0;){
     scanf("%d%d",&a,&v);
-    ins(a,v);
-    printf("%d %d\n", dp->next->num, dp->next->val);
+    add(a-1,v);
+    printf("%d %d\n", get_id()+1, st[0]);
   }
   return 0;
 }
